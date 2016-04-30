c9c39eb65d7d1068d1bd0b0cde7eaeede2aa0881
deecio <d@deecio.com>
2016-04-30 13:25:09 -0500

    Added Canyon Level model, working healthbar

---
+++ Assets/GUI Elements/HealthTestScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthTestScript : MonoBehaviour {
+
+    public static float health = 1.0f;
+    public Image hbImage;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("reduceHealth", 1, 1);
+	}
+
+    private void reduceHealth()
+    {
+        //health -= .1f;
+        hbImage.fillAmount = health;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
