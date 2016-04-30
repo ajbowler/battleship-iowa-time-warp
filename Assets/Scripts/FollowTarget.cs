9b14e96d4d9a281e0922dcdbe7e5430e5eab2a5a
Alex Lende <ajlende@gmail.com>
2016-04-30 16:00:09 -0500

    Moving ship

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour {
+    public GameObject target;
+    public Vector3 offset;
+
+    void Start ()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+	void LateUpdate () {
+        transform.position = target.transform.position + offset;
+	}
+}
--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -3,27 +3,39 @@ using System.Collections;
 
 public class PlayerShip : MonoBehaviour {
 
-    public float speed;
+    public float forwardSpeed;
+    public float lateralSpeed;
+    public float turnSpeed;
 
     private Vector3 moveTo;
+    private Quaternion rotateTo;
 
     void Start()
     {
         moveTo = transform.position;
+        rotateTo = transform.rotation;
     }
 	
 	void Update ()
     {
+        moveTo = transform.position;
+        rotateTo = Quaternion.FromToRotation(Vector3.forward, Vector3.forward);
+
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            moveTo = new Vector3(transform.position.x - 1.0f, transform.position.y, transform.position.z);
+            moveTo.x -= lateralSpeed;
+            rotateTo = Quaternion.FromToRotation(Vector3.forward, new Vector3(-0.4f,0f,1f));
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            moveTo = new Vector3(transform.position.x + 1.0f, transform.position.y, transform.position.z);
+            moveTo.x += lateralSpeed;
+            rotateTo = Quaternion.FromToRotation(Vector3.forward, new Vector3(0.4f,0f,1f));
         }
 
-        transform.position = Vector3.Lerp(transform.position, moveTo, speed * Time.deltaTime);
+        moveTo.z += forwardSpeed;
+
+        transform.position = Vector3.Lerp(transform.position, moveTo, Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotateTo, Time.deltaTime);
 	}
 }
