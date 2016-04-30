a4d0d85b75bb57f5c7afc80d52b7275df007a94a
Andrew Bowler <bowlerandrewj@gmail.com>
2016-04-30 16:55:47 -0500

    Tweak airplane vectors

--- Assets/Scripts/Airplane.cs
+++ Assets/Scripts/Airplane.cs
@@ -4,20 +4,15 @@ using System.Collections;
 public class Airplane : MonoBehaviour {
 
     public float planeSpeed;
-
+    public float targetRadius;
     public GameObject target;
 
 	// Use this for initialization
 	void Start () {
         Transform camTransform = Camera.main.transform;
         Vector3 camForward = camTransform.forward;
-        //Debug.Log("Negative camera: "+ -camForward);
-        //transform.forward = -camForward;//forward.Set(-camForward.x, -camForward.y, -camForward.z);
-        //Debug.Log("Plane forward: "+ transform.forward);
         int xPos = Random.Range(-15, 16);
-        transform.position = new Vector3(transform.position.x + xPos, transform.position.y + 12, transform.position.z + 100);
-        //Vector3 cameraPos = Camera.main.transform.position;
-        //transform.Rotate(new Vector3(0, 1, 0), camTransform.rotation.y);
+        transform.position = new Vector3(transform.position.x + xPos, transform.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
@@ -30,7 +25,7 @@ public class Airplane : MonoBehaviour {
     {
         Vector3 difference = transform.position - target.transform.position;
         Debug.Log(difference);
-        if (difference.magnitude < 50)
+        if (difference.magnitude < targetRadius)
         {
             Debug.Log("FIRIN' WEAPON!");
         }
