1de509504e75f27d78362721f20403b9a6d7a69c
deecio <d@deecio.com>
2016-04-30 22:43:44 -0500

    Added win state.

---
+++ Assets/Scripts/WinScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name.Equals("Player Ship"))
+        {
+            Application.LoadLevel(3);
+        }
+    }
+}
